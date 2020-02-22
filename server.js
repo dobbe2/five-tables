107db0db512652d02ccefeadb3faa834b3833356
Joshua Lo <josh.lol1995@gmail.com>
2020-02-22 13:29:44 -0800

    add reservation logic done

--- server.js
+++ server.js
@@ -4,22 +4,32 @@ const app = express();
 const PORT = 3000;
 
 // List class
-class List {
+class Reservation {
     constructor(name, phoneNumber, Email, uniqueID) {
-        let list = JSON.parse(`{
+        this.list = {
             "name": name,
             "phoneNumber": phoneNumber,
             "email": Email,
             "uniqueID": uniqueID,
-        }`);
-
-        return list;
+        };
     }
 };
 
-let tablesArr = new List;
-let waitlistArr = new List;
-// console.log(tablesArr);
+let tablesArr = [1,2,3,4,5,6];
+let waitlistArr = [];
+
+function addToTables() {
+    if (tablesArr.length <= 5) {
+        const tainer = new Reservation("josh", 123, "123@gmail.com", 69420);
+        tablesArr.push(tainer); // tainer is an object here
+        console.log(tablesArr);
+    } else {
+        const reserve = new Reservation("James", 456, "235@gmail.com", 123454)
+        waitlistArr.push(reserve);
+        console.log(waitlistArr);
+    }
+}
+addToTables();
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -30,10 +40,11 @@ app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "home.html"));
   });
 
-//route for styles.css
+  //route for styles.css
 app.get("/styles.css", function(req, res) {
     res.sendFile(path.join(__dirname, "styles.css"));
 })
+  
 
 // When user does a GET or visits the /api/tables page on our
 // site, return our tables array
@@ -53,3 +64,12 @@ app.get("/api/waitlist", function(req, res) {
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
 });
+
+
+// Backend Logic
+
+// View Reservations
+
+
+// Make Reservation
+    // Form data for 
