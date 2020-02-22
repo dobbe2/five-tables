90c0b07e60243d4bdb36aaed508be850fa2659e6
Dawson Hensel <dawsonhensel@gmail.com>
2020-02-22 11:12:18 -0800

    added outline for server.js routing

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const path = require("path")
+const app = express();
+const PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "home.html"));
+  });
+  
+
+// When user does a GET or visits the /api/tables page on our
+// site, return our tables array
+app.get("/api/tables", function(req, res) {
+    return res.json(tablesArr);
+});
+
+// When user does a GET or visits the /api/waitlist page on our
+// site, return our waitlist array
+
+app.get("/api/waitlist", function(req, res) {
+    return res.json(waitlistArr);
+});
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
